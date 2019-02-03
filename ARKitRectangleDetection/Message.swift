1d89bc148d587f4d1c5e5860010be1ebee9ed21c
Jonathan Kelley <jkelleyrtp@gmail.com>
2019-02-03 09:52:54 -0500

    Initial Commit

---
+++ ARKitRectangleDetection/Message.swift
@@ -0,0 +1,50 @@
+//
+//  Message.swift
+//  ARKitRectangleDetection
+//
+//  Created by Melissa Ludowise on 8/6/17.
+//  Copyright © 2017 Mel Ludowise. All rights reserved.
+//
+
+import Foundation
+
+enum Message {
+    
+    // Help message to instruct the user to move their phone to identify a surface
+    case helpFindSurface
+    
+    // Help message to instruct the user to tap & hold on a rectangle to identify it
+    case helpTapHoldRect
+    
+    // Help message to instruct the user to release their finger to select the rectangle
+    case helpTapReleaseRect
+    
+    // Error message displays when the user taps but no rectangle is detected
+    case errNoRect
+    
+    // Error message displays when a surface cannot be found for the identified rectangle
+    case errNoPlaneForRect
+    
+    case helpImage
+}
+
+extension Message {
+    var localizedString: String {
+        get {
+            switch(self) {
+            case .helpFindSurface:
+                return NSLocalizedString("Move your phone until you see a blue grid covering the surface of your stock.", comment: "")
+            case .helpTapHoldRect:
+                return NSLocalizedString("Tap and hold to select stock.", comment: "")
+            case .helpTapReleaseRect:
+                return NSLocalizedString("Release your finger to finalize your selection.", comment: "")
+            case .errNoRect:
+                return NSLocalizedString("The stock couldn't be identified. Try moving your phone to another angle.", comment: "")
+            case .helpImage:
+                return NSLocalizedString("New Sufrace found", comment: "")
+            case .errNoPlaneForRect:
+                return String(format: NSLocalizedString("The stocks's surface wasn't found. %@", comment: ""), Message.helpFindSurface.localizedString)
+            }
+        }
+    }
+}
---
+++ ARKitRectangleDetection/ViewController.swift
@@ -0,0 +1,451 @@
+//
+//  ViewController.swift
+//  ARKitRectangleDetection
+//
+//  Created by Melissa Ludowise on 8/3/17.
+//  Copyright © 2017 Mel Ludowise. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+import Vision
+
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var messageView: UIView!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var restartButton: UIButton!
+    @IBOutlet weak var debugButton: UIButton!
+    @IBOutlet weak var placeAssemblyButton: UIButton!
+    
+    
+    
+    private var planeNode: SCNNode?
+    private var imageNode: SCNNode?
+    private var tiledpartsnode: SCNNode?
+
+    
+    // MARK: - Internal properties used to identify the rectangle the user is selecting
+    
+    // Displayed rectangle outline
+    private var selectedRectangleOutlineLayer: CAShapeLayer?
+    
+    // Observed rectangle currently being touched
+    private var selectedRectangleObservation: VNRectangleObservation?
+    
+    // The time the current rectangle selection was last updated
+    private var selectedRectangleLastUpdated: Date?
+    
+    // Current touch location
+    private var currTouchLocation: CGPoint?
+    
+    // Gets set to true when actively searching for rectangles in the current frame
+    private var searchingForRectangles = false
+    
+    
+    // MARK: - Rendered items
+    
+    // RectangleNodes with keys for rectangleObservation.uuid
+    private var rectangleNodes = [VNRectangleObservation:RectangleNode]()
+    
+    // Used to lookup SurfaceNodes by planeAnchor and update them
+    //private var surfaceNodes = [ARPlaneAnchor:SurfaceNode]()
+    
+    // MARK: - Debug properties
+    
+    var showDebugOptions = false {
+        didSet {
+            if showDebugOptions {
+                sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+            } else {
+              sceneView.debugOptions = []
+            }
+        }
+    }
+    
+    
+    // MARK: - Message displayed to the user
+    
+    private var message: Message? {
+        didSet {
+            DispatchQueue.main.async {
+                if let message = self.message {
+                    self.messageView.isHidden = false
+                    self.messageLabel.text = message.localizedString
+                    self.messageLabel.numberOfLines = 0
+                    self.messageLabel.sizeToFit()
+                    self.messageLabel.superview?.setNeedsLayout()
+                } else {
+                    self.messageView.isHidden = true
+                }
+            }
+        }
+    }
+    
+    
+    // MARK: - UIViewController
+    
+    override var prefersStatusBarHidden: Bool {
+        get {
+            return true
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegates
+        sceneView.delegate = self
+        
+        // Comment out to disable rectangle tracking
+        sceneView.session.delegate = self
+        
+        // Show world origin and feature points if desired
+        if showDebugOptions {
+            sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        }
+
+        // Enable default lighting
+        sceneView.autoenablesDefaultLighting = true
+        
+        // Create a new scene
+        let scene = SCNScene()
+        sceneView.scene = scene
+        
+        // Don't display message
+        message = nil
+        
+        // Style clear button
+        styleButton(clearButton, localizedTitle: NSLocalizedString("Clear Rects", comment: ""))
+        styleButton(restartButton, localizedTitle: NSLocalizedString("Restart", comment: ""))
+        styleButton(debugButton, localizedTitle: NSLocalizedString("Debug", comment: ""))
+        debugButton.isSelected = showDebugOptions
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+            fatalError("Missing expected asset catalog resources.")
+        }
+
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        // Add previously loaded images to ARScene configuration as detectionImages
+        configuration.detectionImages = referenceImages
+        
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+        // Tell user to find the a surface if we don't know of any
+     //   if surfaceNodes.isEmpty {
+      //      message = .helpFindSurface
+      //  }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first,
+            let currentFrame = sceneView.session.currentFrame else {
+            return
+        }
+        
+        currTouchLocation = touch.location(in: sceneView)
+        findRectangle(locationInScene: currTouchLocation!, frame: currentFrame)
+        message = .helpTapReleaseRect
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Ignore if we're currently searching for a rect
+        if searchingForRectangles {
+            return
+        }
+        
+        guard let touch = touches.first,
+            let currentFrame = sceneView.session.currentFrame else {
+                return
+        }
+        
+        currTouchLocation = touch.location(in: sceneView)
+        findRectangle(locationInScene: currTouchLocation!, frame: currentFrame)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        currTouchLocation = nil
+        message = .helpTapHoldRect
+
+        guard let selectedRect = selectedRectangleObservation else {
+            return
+        }
+        
+        // Create a planeRect and add a RectangleNode
+        addPlaneRect(for: selectedRect)
+    }
+    
+    // MARK: - IBOutlets
+    
+    @IBAction func onInsertModelButton(_ sender: Any) {
+        print("insert model")
+    }
+    
+    @IBAction func onTilePartsButton(_ sender: Any) {
+        print("tile parts")
+    }
+    
+    @IBAction func onStartJobButton(_ sender: Any) {
+        print("start job")
+    }
+    
+    
+    @IBAction func onClearButton(_ sender: Any) {
+        rectangleNodes.forEach({ $1.removeFromParentNode() })
+        rectangleNodes.removeAll()
+    }
+    
+    @IBAction func onRestartButton(_ sender: Any) {
+        // Remove all rectangles
+        rectangleNodes.forEach({ $1.removeFromParentNode() })
+        rectangleNodes.removeAll()
+        
+        // Remove all surfaces and tell session to forget about anchors
+   //     surfaceNodes.forEach { (anchor, surfaceNode) in
+    //        sceneView.session.remove(anchor: anchor)
+      //      surfaceNode.removeFromParentNode()
+      //  }
+       // surfaceNodes.removeAll()
+        
+        // Update message
+        message = .helpFindSurface
+    }
+    
+    @IBAction func onDebugButton(_ sender: Any) {
+        showDebugOptions = !showDebugOptions
+        debugButton.isSelected = showDebugOptions
+        
+        if showDebugOptions {
+            debugButton.layer.backgroundColor = UIColor.yellow.cgColor
+            debugButton.layer.borderColor = UIColor.yellow.cgColor
+        } else {
+            debugButton.layer.backgroundColor = UIColor.black.withAlphaComponent(0.5).cgColor
+            debugButton.layer.borderColor = UIColor.white.cgColor
+        }
+    }
+    
+    // MARK: - ARSessionDelegate
+    
+    // Update selected rectangle if it's been more than 1 second and the screen is still being touched
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        if searchingForRectangles {
+            return
+        }
+        
+        guard let currTouchLocation = currTouchLocation,
+            let currentFrame = sceneView.session.currentFrame else {
+                return
+        }
+        
+        if selectedRectangleLastUpdated?.timeIntervalSinceNow ?? 0 < 1 {
+            return
+        }
+        
+        findRectangle(locationInScene: currTouchLocation, frame: currentFrame)
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+            guard let imageAnchor = anchor as? ARImageAnchor else {
+                return
+            }
+            print("Found detection plane")
+            message = .helpImage
+            
+            // 1. Load plane's scene.
+            let planeScene = SCNScene(named: "art.scnassets/plane.scn")!
+            let planeNode = planeScene.rootNode.childNode(withName: "planeRootNode", recursively: true)!
+            
+            // 2. Calculate size based on planeNode's bounding box.
+            let (min, max) = planeNode.boundingBox
+            let size = SCNVector3Make(max.x - min.x, max.y - min.y, max.z - min.z)
+            
+            // 3. Calculate the ratio of difference between real image and object size.
+            // Ignore Y axis because it will be pointed out of the image.
+            let widthRatio = Float(imageAnchor.referenceImage.physicalSize.width)/size.x
+            let heightRatio = Float(imageAnchor.referenceImage.physicalSize.height)/size.z
+            
+            // Pick smallest value to be sure that object fits into the image.
+            let finalRatio = [widthRatio, heightRatio].min()!
+            
+            // 4. Set transform from imageAnchor data.
+            planeNode.transform = SCNMatrix4(imageAnchor.transform)
+            
+            // 5. Animate appearance by scaling model from 0 to previously calculated value.
+            let appearanceAction = SCNAction.scale(to: CGFloat(finalRatio), duration: 0.4)
+            appearanceAction.timingMode = .easeOut
+            // Set initial scale to 0.
+            planeNode.scale = SCNVector3Make(0.001, 0.001, 0.001)
+            // Add to root node.
+            //sceneView.scene.rootNode.addChildNode(planeNode)
+            // Run the appearance animation.
+            planeNode.runAction(appearanceAction)
+            
+            self.planeNode = planeNode
+            self.imageNode = node
+        
+            let grid = UIImage(named: "art.scnassets/Tiled.png")
+            let planeGeometry = SCNPlane(width: 0.305, height: 0.305)//width: , height: CGFloat(grid!.size.height)/300)
+            let material = SCNMaterial()
+            material.diffuse.contents = grid
+            planeGeometry.materials = [material]
+        
+            //planeGeometry.firstMaterial?.transparency = 0.5
+            let svgNode = SCNNode(geometry: planeGeometry)
+            // svgNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2.0, 1, 0, 0)
+            //svgNode.transform =  SCNMatrix4(imageAnchor.transform)
+            svgNode.transform = SCNMatrix4Mult(SCNMatrix4MakeRotation(-Float.pi/2, 1, 0,0 ), SCNMatrix4(imageAnchor.transform))
+        
+            self.tiledpartsnode = svgNode
+            //sceneView.scene.rootNode.addChildNode(svgNode)
+
+        }
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+
+    }
+    
+    // MARK: - Helper Methods
+    
+    // Updates selectedRectangleObservation with the the rectangle found in the given ARFrame at the given location
+    private func findRectangle(locationInScene location: CGPoint, frame currentFrame: ARFrame) {
+        // Note that we're actively searching for rectangles
+        searchingForRectangles = true
+        selectedRectangleObservation = nil
+        
+        // Perform request on background thread
+        DispatchQueue.global(qos: .background).async {
+            let request = VNDetectRectanglesRequest(completionHandler: { (request, error) in
+                
+                // Jump back onto the main thread
+                DispatchQueue.main.async {
+                    
+                    // Mark that we've finished searching for rectangles
+                    self.searchingForRectangles = false
+                    
+                    // Access the first result in the array after casting the array as a VNClassificationObservation array
+                    guard let observations = request.results as? [VNRectangleObservation],
+                        let _ = observations.first else {
+                            print ("No results")
+                            self.message = .errNoRect
+                            return
+                    }
+                    
+                    print("\(observations.count) rectangles found")
+                    
+                    // Remove outline for selected rectangle
+                    if let layer = self.selectedRectangleOutlineLayer {
+                        layer.removeFromSuperlayer()
+                        self.selectedRectangleOutlineLayer = nil
+                    }
+                    
+                    // Find the rect that overlaps with the given location in sceneView
+                    guard let selectedRect = observations.filter({ (result) -> Bool in
+                        let convertedRect = self.sceneView.convertFromCamera(result.boundingBox)
+                        return convertedRect.contains(location)
+                    }).first else {
+                        print("No results at touch location")
+                        self.message = .errNoRect
+                        return
+                    }
+                    
+                    // Outline selected rectangle
+                    let points = [selectedRect.topLeft, selectedRect.topRight, selectedRect.bottomRight, selectedRect.bottomLeft]
+                    let convertedPoints = points.map { self.sceneView.convertFromCamera($0) }
+                    self.selectedRectangleOutlineLayer = self.drawPolygon(convertedPoints, color: UIColor.red)
+                    self.sceneView.layer.addSublayer(self.selectedRectangleOutlineLayer!)
+                    
+                    // Track the selected rectangle and when it was found
+                    self.selectedRectangleObservation = selectedRect
+                    self.selectedRectangleLastUpdated = Date()
+                    
+                    // Check if the user stopped touching the screen while we were in the background.
+                    // If so, then we should add the planeRect here instead of waiting for touches to end.
+                    if self.currTouchLocation == nil {
+                        // Create a planeRect and add a RectangleNode
+                        self.addPlaneRect(for: selectedRect)
+                    }
+                }
+            })
+            
+            // Don't limit resulting number of observations
+            request.maximumObservations = 0
+            
+            // Perform request
+            let handler = VNImageRequestHandler(cvPixelBuffer: currentFrame.capturedImage, options: [:])
+            try? handler.perform([request])
+        }
+    }
+    
+    private func addPlaneRect(for observedRect: VNRectangleObservation) {
+        // Remove old outline of selected rectangle
+        if let layer = selectedRectangleOutlineLayer {
+            layer.removeFromSuperlayer()
+            selectedRectangleOutlineLayer = nil
+        }
+        
+        // Convert to 3D coordinates
+        guard let planeRectangle = PlaneRectangle(for: observedRect, in: sceneView) else {
+            print("No plane for this rectangle")
+            message = .errNoPlaneForRect
+            return
+        }
+        
+        let rectangleNode = RectangleNode(planeRectangle)
+        rectangleNodes[observedRect] = rectangleNode
+        //sceneView.scene.rootNode.addChildNode(rectangleNode)
+        self.tiledpartsnode!.position = rectangleNode.position
+        sceneView.scene.rootNode.addChildNode(self.tiledpartsnode!)
+
+    }
+    
+    private func drawPolygon(_ points: [CGPoint], color: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        layer.fillColor = nil
+        layer.strokeColor = color.cgColor
+        layer.lineWidth = 2
+        let path = UIBezierPath()
+        path.move(to: points.last!)
+        points.forEach { point in
+            path.addLine(to: point)
+        }
+        layer.path = path.cgPath
+        
+        return layer
+    }
+    
+    private func styleButton(_ button: UIButton, localizedTitle: String?) {
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 4
+        button.setTitle(localizedTitle, for: .normal)
+    }
+}
